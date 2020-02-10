31fe0f5d348282701847abd5394aacf5aa4d6e73
Coolshanlan <joey0201020123@gmail.com>
2020-02-10 21:48:48 +0800

    first commit

---
+++ USB_Lock/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Security.Cryptography;
+namespace USB_Lock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+    public static byte[] GetHash(string inputString)
+    {
+        HashAlgorithm algorithm = MD5.Create(); 
+        return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+    }
+
+    public static string GetHashString(string inputString)
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (byte b in GetHash(inputString))
+            sb.Append(b.ToString("X2"));
+        return sb.ToString();
+    }
+     Password p = new Password();
+     string pass = "99EDB8557AE14E30409A5CFDBE2D7CCE";
+     private void button1_Click(object sender, EventArgs e)
+     {
+         p.ShowDialog();
+         if (GetHashString(p.password) == pass)
+         {
+             if (checkBox1.Checked)
+             {
+                 Microsoft.Win32.Registry.SetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\USBSTOR", "Start", 4, Microsoft.Win32.RegistryValueKind.DWord);
+                }
+             else
+             {
+                 Microsoft.Win32.Registry.SetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\USBSTOR", "Start", 3, Microsoft.Win32.RegistryValueKind.DWord);
+             }
+                MessageBox.Show("Success");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Failed");
+            }
+     }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var value = Microsoft.Win32.Registry.GetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\USBSTOR", "Start", null);
+            if (value.ToString() == "3") checkBox1.Checked = false;
+            else checkBox1.Checked = true;
+        }
+    }
+}
---
+++ USB_Lock/Password.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace USB_Lock
+{
+    partial class Password
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '●';
+            this.textBox1.Size = new System.Drawing.Size(237, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Password
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 79);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Password";
+            this.Text = "Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
